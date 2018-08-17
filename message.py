f83c2779974a6f47552260fba1ebadee461148ae
Gunn <31373828+KPGunner@users.noreply.github.com>
2018-08-17 15:46:31 -0400

    Update message.py

--- message.py
+++ message.py
@@ -8,8 +8,8 @@ client = Client(account_sid, auth_token)
 
 message = client.messages.create(
                               body= message,
-                              from_='+12527729034',
-                              to='+12528762155'
+                              from_='+12345678900',
+                              to='+11234567890'
                           )
 
 print(message.sid)
